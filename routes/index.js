e9eced28f2d209a344334588afca0a28e6bdb322
sarita bagul <saritabagul06@gmail.com>
2023-05-25 01:04:55 +0530

    Adding users controller and router

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
 router.get('/',homeController.home);
+
+router.use('/users',require('./users'))
+router.use('/posts',require('./posts'))
 console.log("router loaded");
 
 module.exports = router;
