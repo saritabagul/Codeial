6fc94f89122ed178d6374ede9022f1bf7fa2fa91
sarita bagul <saritabagul06@gmail.com>
2023-06-10 11:52:09 +0530

    display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,8 @@
+const Post = require('../models/post');
+
+// module.exports.actionName = function(req, res){}
+
+/* previous code
 module.exports.home = function(req, res){
    // return res.end('<h1>Express is up for Codeial!</h1>')
       // console.log(req.cookies);  // requesting cookier from browser
@@ -6,5 +11,29 @@ module.exports.home = function(req, res){
         title:"Home",
    });
 }
+*/
 
-// module.exports.actionName = function(req, res){}
+module.exports.home = async function(req,res){
+   /* here we get only user._id we need to diplay author name also..so need to take whole user object
+   try{
+      const posts = await Post.find({});
+      return res.render('home',{
+         title:"Codeial | Home",
+         posts:posts
+      });
+   }catch(err){
+      console.log('Error to find the posts',err);
+   }
+   */
+
+   try{
+      const posts = await Post.find({}).populate('user').exec();
+      return res.render('home',{
+         title:"Codeial | Home",
+         posts:posts
+      });
+   }catch(err){
+      console.log('Error to find the posts',err);
+   }
+}
+
