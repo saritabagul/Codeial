06a3e993038b6fd802cebffe047d6e9615607d5d
sarita bagul <saritabagul06@gmail.com>
2023-06-14 22:52:34 +0530

    Deleting a post (authorized)

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Posts = require('../models/post');
+const Comments = require('../models/comments');
 
 module.exports.posts = function(req,res){
     res.send('<h1> Posts controller loading</h1>');
@@ -14,6 +15,23 @@ module.exports.create = async function(req,res){
     }catch(err){
         console.log("error in creating a post");return;
     }
-     
+   
 
+}
+
+module.exports.destroy = async function(req,res){
+    try{
+        const post = await Posts.findById(req.params.id);
+        //.id means converting the object _id into string
+        if(post.user == req.user.id){
+            // post.remove();
+            post.deleteOne({post:req.params.id});
+            await Comments.deleteMany({post:req.params.id});
+            return res.redirect('back');
+        }else{
+            return res.redirect('back');
+        }
+    }catch(err){
+        console.log('Record not found',err);
+    }
 }
