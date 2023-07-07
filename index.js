a57b712595b504d8ff6b9ae098331a39e9dd3696
sarita bagul <saritabagul06@gmail.com>
2023-07-07 20:09:27 +0530

    setting up socket.io

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ const flash = require('connect-flash');
 
 const customMware = require('./config/middleware');
 
+//setup the chat server to be used with socket.io
+
+const chatServer = require('http').Server(app);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
+chatServer.listen(5000);
+console.log('chat server is listening on port 5000');
+
 app.use(express.urlencoded());
 app.use(cookieParser());
 const expressLayout = require('express-ejs-layouts');
